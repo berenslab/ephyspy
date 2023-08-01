942d9005fcab92388585aa6813bbc4f02418825c
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2023-08-01 13:53:26 +0200

    fix: rm one version

--- setup.py
+++ setup.py
@@ -3,6 +3,26 @@ import os
 
 NAME = "py_ephys"
 
+EXTRAS = {
+    "dev": [
+    "black", 
+    "isort", 
+    "pyright", 
+    "flake8", 
+    "autoflake",
+    "pre-commit",
+    ]
+}
+
+REQUIRES = [
+"pandas>=1.5.3",
+"numpy>=1.23.5",
+"matplotlib>=3.4.2",
+"scipy>=1.9.1",
+"scikit-learn>=1.1.1", # might get rid of this in the future.
+]
+
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
@@ -12,29 +32,13 @@ with open(os.path.join(here, project_slug, "__version__.py")) as f:
 
 setup(
     name = NAME,
-    version = "0.0.1",
     version=about["__version__"],
     packages = find_packages(include=["py_ephys", "py_ephys.*"], exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     description = "Package to extract summary statistics from electrophysiological data.",
     author = "jnsbck",
     python_requires = ">=3.8",
-    install_requires = [
-    "pandas>=1.5.3",
-    "numpy>=1.23.5",
-    "matplotlib>=3.4.2",
-    "scipy>=1.9.1",
-    "scikit-learn>=1.1.1", # might get rid of this in the future.
-    ],
-    extras_require = {
-        "dev": [
-        "black", 
-        "isort", 
-        "pyright", 
-        "flake8", 
-        "autoflake",
-        "pre-commit",
-        ]
-    },
+    install_requires = REQUIRES,
+    extras_require = EXTRAS,
     tests_require = ["pytest"],
     include_package_data=True,
 )
