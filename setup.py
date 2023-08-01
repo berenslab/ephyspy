8f46b6bc2651569995c9fcb16f16ea4fb6757b1d
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2023-08-01 11:37:09 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=find_packages(include=['py_ephys', 'py_ephys.*']),
     description='Package to extract summary statistics from electrophysiological data.',
     author='jnsbck',
+    python_requires='>=3.8',
     install_requires=[
     'pandas>=1.5.3',
     'numpy>=1.23.5',
@@ -13,6 +14,6 @@ setup(
     'scipy>=1.9.1',
     'sklearn>=0.0', # might get rid of this in the future.
     ],
-    setup_requires=['pytest-runner', 'black'],
+    setup_requires=['black', 'isort', 'pyright', 'flake8', 'autoflake'],
     tests_require=['pytest'],
 )
