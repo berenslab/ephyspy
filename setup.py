8ea0150e1e37aa6daf60d8028010375e9ec5d1c1
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2023-08-01 11:49:21 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py_ephys',
-    version='0.0.1',
-    packages=find_packages(include=['py_ephys', 'py_ephys.*']),
-    description='Package to extract summary statistics from electrophysiological data.',
-    author='jnsbck',
-    python_requires='>=3.8',
-    install_requires=[
+    name = 'py_ephys',
+    version = '0.0.1',
+    packages = find_packages(include=['py_ephys', 'py_ephys.*']),
+    description = 'Package to extract summary statistics from electrophysiological data.',
+    author = 'jnsbck',
+    python_requires = '>=3.8',
+    install_requires = [
     'pandas>=1.5.3',
     'numpy>=1.23.5',
     'matplotlib>=3.4.2',
     'scipy>=1.9.1',
     'scikit-learn>=1.1.1', # might get rid of this in the future.
     ],
-    setup_requires=['black', 'isort', 'pyright', 'flake8', 'autoflake'],
-    tests_require=['pytest'],
+    setup_requires = {
+    'dev': [
+        'black', 
+        'isort', 
+        'pyright', 
+        'flake8', 
+        'autoflake',
+        ],
+    },
+    tests_require = ['pytest'],
 )
