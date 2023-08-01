ff9a415da64a29b56b7b5f0dda946f6b74455767
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2023-08-01 13:45:58 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,37 @@
 from setuptools import setup, find_packages
+import os
+
+NAME = "py_ephys"
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
+project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+with open(os.path.join(here, project_slug, "__version__.py")) as f:
+    exec(f.read(), about)
 
 setup(
-    name = 'py_ephys',
-    version = '0.0.1',
+    name = NAME,
+    version = "0.0.1",
     version=about["__version__"],
-    packages = find_packages(include=['py_ephys', 'py_ephys.*'], exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
-    description = 'Package to extract summary statistics from electrophysiological data.',
-    author = 'jnsbck',
-    python_requires = '>=3.8',
+    packages = find_packages(include=["py_ephys", "py_ephys.*"], exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    description = "Package to extract summary statistics from electrophysiological data.",
+    author = "jnsbck",
+    python_requires = ">=3.8",
     install_requires = [
-    'pandas>=1.5.3',
-    'numpy>=1.23.5',
-    'matplotlib>=3.4.2',
-    'scipy>=1.9.1',
-    'scikit-learn>=1.1.1', # might get rid of this in the future.
+    "pandas>=1.5.3",
+    "numpy>=1.23.5",
+    "matplotlib>=3.4.2",
+    "scipy>=1.9.1",
+    "scikit-learn>=1.1.1", # might get rid of this in the future.
     ],
     extras_require = [
-    'black', 
-    'isort', 
-    'pyright', 
-    'flake8', 
-    'autoflake',
+    "black", 
+    "isort", 
+    "pyright", 
+    "flake8", 
+    "autoflake",
     ],
-    tests_require = ['pytest'],
+    tests_require = ["pytest"],
+    include_package_data=True,
 )
