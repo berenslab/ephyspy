502efc75a0542c35a788cd2800dde224d31b3171
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2023-08-24 17:06:28 +0200

    add: add tests for diagnostic plotting

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -1,16 +1,78 @@
 import matplotlib.pyplot as plt
 import pytest
 
+from ephyspy.features import *
+from ephyspy.plot import plot_spike_feature, plottable_spike_features
 from tests.helpers import depol_test_sweep, hyperpol_test_sweep, test_sweepset
+
+#####################
+### general tests ###
+#####################
+
+
+@pytest.mark.parametrize("show_stim", [True, False])
+@pytest.mark.parametrize(
+    "sweep_or_sweepset",
+    [depol_test_sweep, hyperpol_test_sweep, test_sweepset],
+    ids=["depol", "hyperpol", "sweepset"],
+)
+def test_sweep_plotting(sweep_or_sweepset, show_stim):
+    ax = sweep_or_sweepset.plot(show_stimulus=show_stim)
+    if isinstance(ax, np.ndarray):
+        assert all(
+            [isinstance(a, plt.Axes) for a in ax]
+        ), "Plot does not return an Axes object."
+    else:
+        assert isinstance(ax, plt.Axes), "Plot does not return an Axes object."
+
 
 ############################
 ### spike level features ###
 ############################
 
 
+@pytest.mark.parametrize("ft", plottable_spike_features)
+@pytest.mark.parametrize("show_sw", [True, False])
+@pytest.mark.parametrize("show_stim", [True, False])
+@pytest.mark.parametrize(
+    "sweep", [depol_test_sweep, hyperpol_test_sweep], ids=["depol", "hyperpol"]
+)
+def test_spike_feature(ft, sweep, show_sw, show_stim):
+    """Test spike plotting for hyperpolarizing and depolarizing sweeps."""
+    ax = plot_spike_feature(sweep, ft, show_sweep=show_sw, show_stimulus=show_stim)
+    if isinstance(ax, np.ndarray):
+        assert all(
+            [isinstance(a, plt.Axes) for a in ax]
+        ), "Plot does not return an Axes object."
+    else:
+        assert isinstance(ax, plt.Axes), "Plot does not return an Axes object."
+
+
 ############################
 ### sweep level features ###
 ############################
+
+depol_test_sweep.add_features(available_spike_features())
+hyperpol_test_sweep.add_features(available_spike_features())
+
+
+@pytest.mark.parametrize("show_sw", [True, False])
+@pytest.mark.parametrize("show_stim", [True, False])
+@pytest.mark.parametrize(
+    "Ft", available_sweep_features().values(), ids=available_sweep_features().keys()
+)
+@pytest.mark.parametrize(
+    "sweep", [depol_test_sweep, hyperpol_test_sweep], ids=["depol", "hyperpol"]
+)
+def test_sweep_feature(Ft, sweep, show_sw, show_stim):
+    ft = Ft(sweep)
+    ax = ft.plot(show_sweep=show_sw, show_stimulus=show_stim)
+    if isinstance(ax, np.ndarray):
+        assert all(
+            [isinstance(a, plt.Axes) for a in ax]
+        ), "Plot does not return an Axes object."
+    else:
+        assert isinstance(ax, plt.Axes), "Plot does not return an Axes object."
 
 
 ###############################
