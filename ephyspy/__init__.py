1485fa091856a8638fa9ad06095aa0907d43bbef
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2023-09-06 13:42:23 +0200

    fix: fix version import

--- ephyspy/__init__.py
+++ ephyspy/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
+from ephyspy.__version__ import __version__
 from ephyspy.analysis import *
 from ephyspy.features.base import *
 from ephyspy.features.spike_features import *
