fix/add: fix a bunch of bugs related to ramp and fp feature computation. Add wildness_num_ap feature

ephyspy/features/spike_features.py
@@ -160,7 +160,7 @@ class Spike_AP_amp(SpikeFeature):
         v_peak = self.lookup_spike_feature("peak_v", recompute=recompute)
         threshold_v = self.lookup_spike_feature("threshold_v", recompute=recompute)
         peak_height = v_peak - threshold_v
-        return peak_height if len(v_peak) > 0 else np.array([])
+        return peak_height if len(v_peak) > 0 else np.array([], dtype=int)
 
     def _plot(self, ax: Optional[Axes] = None, selected_idxs=None, **kwargs) -> Axes:
         if has_spike_feature(self.data, "threshold_v"):
@@ -468,7 +468,7 @@ class Spike_ISI(SpikeFeature):
         elif len(spike_times) == 1:
             return np.array([float("nan")])
         else:
-            return np.array([])
+            return np.array([], dtype=int)
 
     def _plot(self, ax: Optional[Axes] = None, selected_idxs=None, **kwargs) -> Axes:
         if has_spike_feature(self.data, "isi"):

ephyspy/features/sweep_features.py
@@ -559,6 +559,9 @@ class Sweep_AP_amp_slope(SweepFeature):
 
         peak_t = peak_t[stim_window]
         peak_v = peak_v[stim_window]
+        valid = ~np.isnan(peak_v)
+        peak_v = peak_v[valid]
+        peak_t = peak_t[valid]
 
         if len(peak_v) > 5:
             y = lambda x, m, b: m * x + b
@@ -1670,15 +1673,15 @@ class Sweep_Wildness(SweepFeature):
             peak_v = self.lookup_spike_feature("peak_v", recompute=recompute)
             stim_window = where_between(peak_t, onset, end)
 
-            i_wild_spikes = peak_idx[~stim_window]
+            idx_wild_spikes = peak_idx[~stim_window]
             t_wild_spikes = peak_t[~stim_window]
             v_wild_spikes = peak_v[~stim_window]
-            if len(i_wild_spikes) > 0:
-                num_wild_spikes = len(i_wild_spikes)
+            if len(idx_wild_spikes) > 0:
+                num_wild_spikes = len(idx_wild_spikes)
                 if store_diagnostics:
                     self._update_diagnostics(
                         {
-                            "i_wild_spikes": i_wild_spikes,
+                            "idx_wild_spikes": idx_wild_spikes,
                             "t_wild_spikes": t_wild_spikes,
                             "v_wild_spikes": v_wild_spikes,
                         }
@@ -1738,16 +1741,17 @@ class APSweepFeature(SweepFeature):
 
             # include sanity check?
             # first = np.array([], dtype=int)
-            # if np.any(stim_window):
-            #     ap_ft = self.lookup_spike_feature(self.name)[stim_window]
-            #     mu = np.nanmean(ap_ft)
-            #     bound = 5 * np.nanstd(ap_ft)
-            #     is_ok = np.logical_and((mu - bound) < ap_ft, ap_ft < (mu + bound))
-            #     if np.any(is_ok):
-            #         first = np.where(stim_window)[0][is_ok][0]
+            if np.any(stim_window):
+                #     ap_ft = self.lookup_spike_feature(self.name)[stim_window]
+                #     mu = np.nanmean(ap_ft)
+                #     bound = 5 * np.nanstd(ap_ft)
+                #     is_ok = np.logical_and((mu - bound) < ap_ft, ap_ft < (mu + bound))
+                #     if np.any(is_ok):
+                #         first = np.where(stim_window)[0][is_ok][0]
 
-            #     return first
-            return np.where(stim_window)[0][0]
+                #     return first
+                return np.where(stim_window)[0][0]
+            return np.array([], dtype=int)
         else:
             return self.ap_selector(data)
 
@@ -2003,4 +2007,11 @@ class Sweep_ISI(APSweepFeature):
         # ISI_{i} = t_{i} - t_{i-1}
         # therefore ISI_{1} = t_1 - t_0 = t_1 - NaN -> defined as 0
         # first actual ISI is from 1st to 2nd spike, hence the +1
-        return super()._select(data) + 1
+        isi = self.lookup_spike_feature("isi")
+        threshold_t = self.lookup_spike_feature("threshold_t")
+        onset = self.lookup_sweep_feature("stim_onset")
+        end = self.lookup_sweep_feature("stim_end")
+        stim_window = where_between(threshold_t, onset, end)
+        if np.sum(stim_window) > 1:
+            return super()._select(data) + 1
+        return np.array([], dtype=int)

ephyspy/features/sweepset_features.py
@@ -21,6 +21,7 @@ from typing import Dict, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import ndarray
 import pandas as pd
 from matplotlib.axes import Axes
 from sklearn import linear_model
@@ -105,14 +106,16 @@ class APFeature(SweepSetFeature):
             if not peaks_to_low:
                 idx = np.where(is_depol & has_spikes)[0][0]
             else:
-                idx = np.array([])
-        if stimulus_type(self.data) == "ramp":
+                idx = np.array([], dtype=int)
+        elif stimulus_type(self.data) == "ramp":
             ap_peak = self.lookup_sweep_feature("ap_peak")
             where_peak = ~np.isnan(ap_peak)
             if np.any(where_peak):
                 idx = np.where(where_peak)[0][0]
             else:
-                idx = np.array([])
+                idx = np.array([], dtype=int)
+        else:
+            idx = np.array([], dtype=int)
 
         self._update_diagnostics(
             {"selected_idx": idx, "selection": parse_desc(self._select)}
@@ -202,6 +205,7 @@ class APsFeature(SweepSetFeature):
         wildness = self.lookup_sweep_feature("wildness")
         is_non_wild = np.isnan(wildness)
         idx = pd.Series(num_spikes)[is_non_wild].idxmax()
+        idx = np.array([], dtype=int) if np.isnan(idx) else idx
 
         self._update_diagnostics(
             {
@@ -239,8 +243,8 @@ class First5MedianFeature(SweepSetFeature):
             self._update_diagnostics({"first5_idx": where_value})
             return first5
 
-        self._update_diagnostics({"first5_idx": np.array([])})
-        return np.array([])
+        self._update_diagnostics({"first5_idx": np.array([], dtype=int)})
+        return np.array([], dtype=int)
 
     def _aggregate(self, fts):
         """Compute aggregate metric on subset of sweeps.
@@ -306,7 +310,11 @@ class SweepSet_AP_latency(SweepSetFeature):
         """
         is_depol = self.lookup_sweep_feature("stim_amp") > 0
         ap_latency = self.lookup_sweep_feature("ap_latency")
-        idx = pd.Series(is_depol).index[is_depol & ~np.isnan(ap_latency)][0]
+        idxs = pd.Series(is_depol).index[is_depol & ~np.isnan(ap_latency)]
+        if len(idxs) > 0:
+            idx = idxs[0]
+        else:
+            idx = np.array([], dtype=int)
         self._update_diagnostics(
             {
                 "selected_idx": idx,
@@ -1084,6 +1092,34 @@ class SweepSet_AP_UDR(APFeature):
         super().__init__(swft.Sweep_AP_UDR, data=data, compute_at_init=compute_at_init)
 
 
+class SweepSet_Num_wild_APs(SweepSetFeature):
+    """Obtain sweepset level number of wild APs feature.
+
+    description: Max number of APs outside of stimulus window.
+    depends on: /.
+    units: /.
+    """
+
+    def __init__(self, data=None, compute_at_init=True):
+        super().__init__(
+            swft.Sweep_Wildness,
+            data=data,
+            compute_at_init=compute_at_init,
+            name="num_wild_aps",
+        )
+        self.parse_docstring()
+
+    def _select(self, fts: ndarray) -> ndarray:
+        """Select representative sweep and use its wildness feature to represent
+        the entire sweepset.
+
+        description: argmax.
+        """
+        if np.any(~np.isnan(fts)):
+            return np.nanargmax(fts)
+        return np.array([], dtype=int)
+
+
 class SweepSet_Wildness(SweepSetFeature):
     """Obtain sweepset level wildness feature.
 
@@ -1092,7 +1128,7 @@ class SweepSet_Wildness(SweepSetFeature):
     highest firing trace as defined above (without any APs outside the
     stimulation window)
     depends on: Sweep_Num_AP.
-    units: mV.
+    units: /.
     """
 
     def __init__(self, data=None, compute_at_init=True):
@@ -1107,9 +1143,10 @@ class SweepSet_Wildness(SweepSetFeature):
         num_ap = self.lookup_sweep_feature("num_ap", recompute=recompute)
         wildness = self.lookup_sweep_feature("wildness", recompute=recompute)
         is_wild = ~np.isnan(wildness)
+        not_nan = ~np.isnan(num_ap)
 
-        if np.any(is_wild):
-            wildness = num_ap[is_wild].max() - num_ap[~is_wild].max()
+        if np.any(is_wild) and np.any(not_nan):
+            wildness = num_ap[is_wild].max() - num_ap[~is_wild & not_nan].max()
         else:
             wildness = float("nan")
         return wildness